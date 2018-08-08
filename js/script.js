094d0918a95b8a69355c8df48df03782464d4f35
Eric Conner and Michael Donovan <eric@>
2018-08-08 15:56:59 -0700

    Add functionality to numeral buttons

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
+var leftSideNumber = 0
+var currentOperation = ""
+
+operators =
+{
+  "+" : function(num1,num2) {
+    return parseFloat(num1)+parseFloat(num2)
+  },
+  "-" : function(num1,num2) {
+    return parseFloat(num1)-parseFloat(num2)
+  },
+  "*" : function(num1,num2) {
+    return parseFloat(num1)*parseFloat(num2)
+  },
+  "÷" : function(num1,num2) {
+    return parseFloat(num1)/parseFloat(num2)
+  }
+}
+
+
+
 $(document).ready(function(){
+  printToDisplay("0")
   setButtonIDs()
 
 
@@ -6,19 +28,75 @@ $(document).ready(function(){
   var minusButton = $("#button--")
   var multiplyButton = $("#button-m")
   var divideButton = $("#button-÷")
+
+  var clearButton = $("#button-CLEAR")
+  var equalsButton = $("#button-e")
+
   addButton.click(function(){
-    console.log("clicked add")
+    var displayedNumber = $("#display").text()
+    leftSideNumber = parseFloat(displayedNumber)
+    $('#number-1').val(leftSideNumber)
+    $("#display").text("0")
+    currentOperation = operators["+"]
   })
   minusButton.click(function(){
+
     console.log("clicked subtract")
+    var displayedNumber = $("#display").text()
+    leftSideNumber = parseFloat(displayedNumber)
+    $('#number-1').val(leftSideNumber)
+    $("#display").text("0")
+    currentOperation = operators["-"]
   })
   multiplyButton.click(function(){
+
     console.log("clicked mult")
+    var displayedNumber = $("#display").text()
+    leftSideNumber = parseFloat(displayedNumber)
+    $('#number-1').val(leftSideNumber)
+    $("#display").text("0")
+    currentOperation = operators["*"]
   })
   divideButton.click(function(){
+
     console.log("clicked divide")
+    var displayedNumber = $("#display").text()
+    leftSideNumber = parseFloat(displayedNumber)
+    $('#number-1').val(leftSideNumber)
+    $("#display").text("0")
+    currentOperation = operators["÷"]
   })
+
+  $(".num-key").click(function(){
+    var toPrint = $(this).text()
+    if ($('#display').text()==="0") {
+      $('#display').text("")
+    }
+    addToDisplay(toPrint)
+  })
+
+  clearButton.click(function(){
+    currentResult = 0
+    $("#display").text("0")
+  })
+
+  equalsButton.click(function(){
+    console.log("clicked equals")
+    var rightSideNumber = $('#display').text()
+    var result = currentOperation(leftSideNumber,rightSideNumber)
+    leftSideNumber = rightSideNumber
+    printToDisplay(result)
+  })
+
+
 })
+
+function printToDisplay(string) {
+  $('#display').text(string)
+}
+function addToDisplay(numeral){
+  $('#display').append(numeral)
+}
 
 function setButtonIDs() {
   $('.calc-key').each(function(){
@@ -28,6 +106,9 @@ function setButtonIDs() {
     }
     if (newID==="button-*") {
       newID = "button-m"
+    }
+    if (newID==="button-=") {
+      newID = "button-e"
     }
     $(this).attr("id",newID)
   })
