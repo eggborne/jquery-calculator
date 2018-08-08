b1df90aa1b72301c41cfbb29bc30f1c291112c6a
Eric Conner and Michael Donovan <michael@>
2018-08-08 14:14:21 -0700

    Create input forms and basic button function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
 $(document).ready(function(){
+  setButtonIDs()
 
+
+  var addButton = $("#button-a")
+  var minusButton = $("#button--")
+  var multiplyButton = $("#button-m")
+  var divideButton = $("#button-÷")
+  addButton.click(function(){
+    console.log("clicked add")
+  })
+  minusButton.click(function(){
+    console.log("clicked subtract")
+  })
+  multiplyButton.click(function(){
+    console.log("clicked mult")
+  })
+  divideButton.click(function(){
+    console.log("clicked divide")
+  })
 })
+
+function setButtonIDs() {
+  $('.calc-key').each(function(){
+    var newID = "button-" + $(this).html()
+    if (newID==="button-+") {
+      newID = "button-a"
+    }
+    if (newID==="button-*") {
+      newID = "button-m"
+    }
+    $(this).attr("id",newID)
+  })
+}
